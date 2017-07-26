18b92f05e38459149f13e199ece00f0e40f4ecb7
Stephen <stephen.frx@gmail.com>
2017-07-26 21:43:23 +0800

    add login log

---
+++ app/Listeners/LogSuccessfulLogin.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Listeners;
+
+use Carbon\Carbon;
+use Illuminate\Auth\Events\Login;
+use Illuminate\Http\Request;
+
+class LogSuccessfulLogin {
+	/**
+	 * Create the event listener.
+	 *
+	 * @param Illuminate\Http\Request $request
+	 * @return void
+	 */
+	public function __construct(Request $request) {
+		$this->request = $request;
+	}
+
+	/**
+	 * Handle the event.
+	 *
+	 * @param  Login  $event
+	 * @return void
+	 */
+	public function handle(Login $event) {
+		$user = $event->user;
+
+		$user->last_login_at = Carbon::now();
+		$user->last_login_ip = $this->request->ip();
+
+		$user->save();
+	}
+}
